cac7b714cf28d987946922d4453339c6d4fcb104
victor hugo furtuosos soares <vhfurtuoso@gmail.com>
2017-05-05 21:42:19 -0300

    #9326 implementado metodos save e remove do service do aluno

--- app/aluno/services/alunoService.js
+++ app/aluno/services/alunoService.js
@@ -1,15 +1,24 @@
 aluno.factory("alunoService",alunoService);
-
-function alunoService(){
+alunos = [];
+function alunoService(uuid){
   function getAll() {
     return this.alunos;
   }
 
-  function save(modalidade) {
-
+  function save(aluno) {
+    alunos.push(aluno);
   }
 
   function remove(id) {
+    for (var index = 0; index < alunos.length; index++) {
+      if (alunos[index].id = aluno.id) {
+        break;
+      }
+    for (var y = index; y < alunos.length; y++) {
+      alunos[y] = alunos[y+1];      
+    }
+      
+    }
 
   }
 
